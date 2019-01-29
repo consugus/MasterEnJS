c1a86acdbc97b5a001515e1692242549ea0daed1
Gustavo <consugus@gmail.com>
2019-01-29 13:03:35 -0300

    Accordion with jQuery UI done

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
     });
 
     // Carga dinámica de Posts
-    var posts = [{
+    if(window.location.pathname.includes("index.html")){
+        var posts = [{
             title: "Prueba de título 1",
             date: "Publicado el día " + moment().format('dddd DD') + " de " + moment().format('MMMM') + " de " + moment().format('YYYY'),
             content: "Enim quia illo iure sit voluptatibus numquam perferendis quidem iste. Quod quae unde. Occaecati ullam recusandae. Exercitationem corrupti iste ipsam."
@@ -35,18 +36,19 @@ $(document).ready(function () {
         }
     ];
 
-    posts.forEach((item, index) => {
-        var post = `
-            <article class="post">
-                <h2>${item.title}</h2>
-                <p class="date">${item.date}</p>
-                <p class="text"> ${item.content}
-                    <a href="#" type="button" class="readMore">Leer mas...</a>
-                </p>
-            </article><!-- end article -->
-        `;
-        $('#posts').append(post);
-    }); // Fin carga dinámica de posts
+        posts.forEach((item, index) => {
+            var post = `
+                <article class="post">
+                    <h2>${item.title}</h2>
+                    <p class="date">${item.date}</p>
+                    <p class="text"> ${item.content}
+                        <a href="#" type="button" class="readMore">Leer mas...</a>
+                    </p>
+                </article><!-- end article -->
+            `;
+            $('#posts').append(post);
+        }); // Fin carga dinámica de posts
+    };
 
     // Cambio de tema
     var theme = $('#theme');
@@ -67,6 +69,37 @@ $(document).ready(function () {
             scrollTop: 0
         }, 500);
     }); // end Ir arriba
+
+
+    // Login falso
+    $('#login form').submit(function(){
+        var name =$('#form_name').val();
+        var email =$('#form_email').val();
+        var password =$('#form_password').val();
+
+        localStorage.setItem('form_name', name);
+        localStorage.setItem('form_email', email);
+        localStorage.setItem('form_password', password);
+    });
+
+    var form_name = localStorage.getItem('form_name');
+    if(form_name != null && form_name != undefined){
+        var about_parrafo = $('#about p');
+        about_parrafo.html('<br><strong>Bienvenido ' + form_name + '</strong>');
+        about_parrafo.append('<a href=""# type="button" class="readMore" id="logout"> Cerrar Sesion</a>');
+        $('#login').hide();
+
+        $('#logout').click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    }; // end Login falso
+
+
+    if(window.location.pathname.includes("about.html")){
+        $('#accordion').accordion();
+    };
+
 
 
 }); // end $(document).ready()
